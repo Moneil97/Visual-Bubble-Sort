61c0f6cc8c2291b7c193eaf040da6704451ae83c
Moneil <cam8059@yahoo.com>
2015-03-25 17:07:00 -0500

    Revert

--- src/VisualBubbleSort.java
+++ src/VisualBubbleSort.java
@@ -1,11 +1,23 @@
-import java.awt.Color;
-import java.awt.Font;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-public abstract class Sorter {
 
+<<<<<<< HEAD
 	private JPanel panel;
 	private int blockWidth, blockAmount;
 	private float[] blocks;
@@ -23,110 +35,163 @@ public abstract class Sorter {
 		blocks = new float[blockAmount];
 		generateBlocks();
 	}
+=======
+@SuppressWarnings("serial")
+public class VisualBubbleSort extends JFrame{
+>>>>>>> parent of 0de2d51... Improved Painting Algorithm
 	
-	private void generateBlocks() {
-		for (int i=0; i < blockAmount; i++)
-			blocks[i] = (float) Math.random();
-	}
+	private Options ops;
+	private JPanel panel;
+	private Sorter sorter;
+	private JSlider elementsSlider;
+	private JButton btnStart;
+	private JButton btnStop;
+	protected static int sleepCounter = 50;
 
-	public void draw(Graphics2D g){
+	public VisualBubbleSort() {
 		
+		setTitle("Visual Bubble Sort [By: Cameron O'Neil]");
+		setSize(800,600);
+		setExtendedState(MAXIMIZED_BOTH);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
 		
-		for (int i=0; i < blocks.length; i++){
-			int blockHeight = Math.round(blocks[i] * panel.getHeight());
+		panel = new JPanel(){
 			
-			g.setColor(!running ? Color.green : (i == currentBlock || i == currentBlock +1 ? Color.blue : (i < blocks.length-done ? Color.red : Color.green)));
-			g.fillRect(i*blockWidth, panel.getHeight()- blockHeight, blockWidth, blockHeight);
-			
-			if(blockWidth > 2){
-				g.setColor(Color.black);
-				g.drawRect(i*blockWidth, panel.getHeight()- blockHeight, blockWidth, blockHeight);
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				
+				if (sorter != null) sorter.draw((Graphics2D) g);
+				
 			}
-		}
+			
+		};
 		
-		g.setColor(Color.blue);
-		g.setFont(new Font("Arial", Font.PLAIN, 14));
-		g.drawString("Comparisons: " + comparisons, 10, 20);
 		
-	}
-
-	public void setBlockWidth(int i) {
-		blockWidth = i;
-		blockAmount = panel.getWidth()/blockWidth;
-		blocks = new float[blockAmount];
-		generateBlocks();
-	}
-
-	long comparisons = 0;
-	
-	public void start() {
-
-		thread = new Thread(new Runnable() {
-
+		getContentPane().add(panel, BorderLayout.CENTER);
+		ops = new Options();
+		add(ops, BorderLayout.SOUTH);
+		setVisible(true);
+		
+		EventQueue.invokeLater(new Runnable() {
 			@Override
 			public void run() {
+				sorter = new Sorter(panel){
 
-				try {
-					
-					running = true;
-					comparisons = 0;
-
-					boolean noChange = false;
-
-					while (!noChange) {
-
-						noChange = true;
-
-						for (int i = 0; i < blocks.length - 1 - done; i++) {
-
-							currentBlock = i;
-							comparisons++;
-
-							if (blocks[i] <= blocks[i + 1])
-								continue;
-							else {
-								float temp = blocks[i];
-								blocks[i] = blocks[i + 1];
-								blocks[i + 1] = temp;
-								noChange = false;
-							}
-
-							panel.repaint();
-
-							Thread.sleep(VisualBubbleSort.sleepCounter);
-
-						}
-						done++;
+					@Override
+					void onComplete() {
+						resetStopButtons();
 					}
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}finally{
-					done = 0;
-					currentBlock = 0;
-					running = false;
-					thread = null;
-					onComplete();
-					panel.repaint();
-					say("done");
-				}
+					
+				};
+				VisualBubbleSort.this.repaint();
 			}
 		});
+		
+	}
+	
+	class Options extends JPanel{
+		
+		public Options() {
+			this.setLayout(new GridLayout(2,0));
+			
+			JPanel top = new JPanel();
+			top.setLayout(new GridLayout(0,2));
+			top.setBorder(new EmptyBorder(10, 10, 10, 10));
+			this.add(top);
+			JPanel left = new JPanel();
+			top.add(left);
+			left.setLayout(new BorderLayout());
+			
+			JLabel lblElementsToSort = new JLabel("Elements to Sort:");
+			left.add(lblElementsToSort, BorderLayout.WEST);
+			
+			elementsSlider = new JSlider(1,100, 50);
+			elementsSlider.setInverted(true);
+			elementsSlider.setMajorTickSpacing(10);
+			elementsSlider.setMinorTickSpacing(1);
+			elementsSlider.setPaintTicks(true);
+			elementsSlider.addChangeListener(new ChangeListener() {
+				@Override
+				public void stateChanged(ChangeEvent e) {
+					sorter.setBlockWidth(elementsSlider.getValue());
+					VisualBubbleSort.this.repaint();
+				}
+			});
+			left.add(elementsSlider);
+			JPanel right = new JPanel();
+			top.add(right);
+			right.setLayout(new BorderLayout());
+			
+			JLabel lblSpeed = new JLabel("Delay:");
+			right.add(lblSpeed, BorderLayout.WEST);
+			
+			JSlider timeSlider = new JSlider(0,1000, 50);
+			timeSlider.setMajorTickSpacing(100);
+			timeSlider.setMinorTickSpacing(1);
+			timeSlider.setPaintTicks(true);
+			timeSlider.addChangeListener(new ChangeListener() {
+				@Override
+				public void stateChanged(ChangeEvent e) {
+					sleepCounter = timeSlider.getValue();
+				}
+			});
+			right.add(timeSlider);
+			
+			JPanel bottom = new JPanel();
+			bottom.setLayout(new GridLayout(0,2));
+			bottom.setBorder(new EmptyBorder(0, 10, 10, 10));
+			this.add(bottom);
+			
+			btnStart = new JButton("Start");
+			btnStop = new JButton("Cancel");
+			btnStart.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					
+					sorter.setBlockWidth(elementsSlider.getValue());
+					sorter.start();
+					elementsSlider.setEnabled(false);
+					btnStop.setEnabled(true);	
+					btnStart.setEnabled(false);
+					VisualBubbleSort.this.repaint();
+				}
+			});
+			btnStop.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					sorter.stop();
+					resetStopButtons();
+				}
+			});
+			btnStop.setEnabled(false);
+			
+			bottom.add(btnStart);
+			bottom.add(btnStop);
+		}
 
-		thread.start();
 	}
 	
-	public void stop(){
-		try{
-			thread.interrupt();
-		}catch(Exception e){
-			e.printStackTrace();
-		};
+	protected void resetStopButtons() {
+		elementsSlider.setEnabled(true);
+		btnStart.setEnabled(true);	
+		btnStop.setEnabled(false);	
 	}
-	
-	
 	
 	public static void say(Object s){
 		System.out.println(s);
 	}
-	
+
+	public static void main(String[] args) {
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}catch(Exception e){};
+		
+		new VisualBubbleSort();
+	}
+
 }
